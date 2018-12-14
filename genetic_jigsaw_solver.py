0f6fc439fe222d44f551c47af8f1436c18a7eefa
KrisNguyen135 <nguyenminhquan135@gmail.com>
2018-12-13 20:08:54 -0500

    downloaded input images

---
+++ genetic_jigsaw_solver.py
@@ -0,0 +1,5 @@
+class JigsawSover:
+    def __init__(self, img):
+        self.img = img
+
+    
---
+++ main.py
@@ -0,0 +1,7 @@
+import numpy
+from skimage import io
+
+if __name__ == '__main__':
+    # read in an image
+    img = io.imread('pictures/michelangelo-creation-of-adam.jpg')
+    
